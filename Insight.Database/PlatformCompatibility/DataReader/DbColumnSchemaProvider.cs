a6e40ba5d7b7798a2be6787e049d6a8c73ffa528
Dave Deschenes <Dave.Deschenes@EF.com>
2016-10-10 19:32:28 -0400

    renamed column schema to dbColumnSchema and added inheritance from DbColumn

--- Insight.Database/PlatformCompatibility/DataReader/DbColumnSchemaProvider.cs
+++ Insight.Database/PlatformCompatibility/DataReader/DbColumnSchemaProvider.cs
@@ -1,80 +1,99 @@
 ﻿using System;
+using System.Linq;
+using System.Data.Common;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Data;
-using System.Data.Common;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Insight.Database.PlatformCompatibility.DataReader;
+using System.Runtime.CompilerServices;
+
 
 namespace Insight.Database.PlatformCompatibility.DataReader
 {
 
 #if NETCORE && !COREONDESK
 
-	/// <summary>Provides platform independent schema information for DbColumn Schemas</summary>
-	internal class DbColumnSchemaProvider : ColumnSchemaProviderBase
-	{
-		Dictionary<string, DbColumn> _columnSchemas;
+    /// <summary>Provides platform independent schema information for DbColumn Schemas</summary>
+    internal class DbColumnSchemaProvider : ColumnSchemaProviderBase
+    {
+        public DbColumnSchemaProvider(IEnumerable<DbColumn> argDbColumns)
+            : base(GenerateColumnSchemas(argDbColumns))
+        {
+        }
 
-		public DbColumnSchemaProvider(IEnumerable<DbColumn> argDbColumns)
-			: base(GenerateColumnSchemas(argDbColumns))
-		{
-			_columnSchemas = new Dictionary<string, DbColumn>();
+        private static IColumnSchema[] GenerateColumnSchemas(IEnumerable<DbColumn> argDbColumns)
+        {
+            return argDbColumns.Select(dbColumn => (IColumnSchema)new DbColumnSchema(dbColumn)).ToArray();
+        }
 
-			foreach (DbColumn dbColumn in argDbColumns.ToList())
-			{
-				if (_columnSchemas.ContainsKey(dbColumn.ColumnName) == false)
-					_columnSchemas.Add(dbColumn.ColumnName, dbColumn);
-			}
-		}
+        internal ReadOnlyCollection<DbColumn> GetColumnSchema()
+        {
+            // TODO Review.  This honors removed columns but does not respect changes to other attributes
+            // because its a second read only copy of the data.  We should subclass DbColumn and use it to hold the data?
 
-		private static IColumnSchema[] GenerateColumnSchemas(IEnumerable<DbColumn> argDbColumns)
-		{
-			return argDbColumns.Select(dbColumn => (IColumnSchema)new ColumnSchema(dbColumn)).ToArray();
-		}
+            return new ReadOnlyCollection<DbColumn>(this.Cast<DbColumn>().ToList());
+        }
 
-		internal ReadOnlyCollection<DbColumn> GetColumnSchema()
-		{
-			// TODO Review.  This honors removed columns but does not respect changes to other attributes
-			// because its a second read only copy of the data.  We should subclass DbColumn and use it to hold the data?
+        private class DbColumnSchema : DbColumn, IColumnSchema
+        {
+            private readonly DbColumn _dbColumn;
 
-			var list = new List<DbColumn>();
+            public DbColumnSchema(DbColumn dbColumn)
+            {
+                AllowDBNull = dbColumn.AllowDBNull;
+                BaseCatalogName = dbColumn.BaseCatalogName;
+                BaseColumnName = dbColumn.BaseColumnName;
+                BaseSchemaName = dbColumn.BaseSchemaName;
+                BaseServerName = dbColumn.BaseServerName;
+                BaseTableName = dbColumn.BaseTableName;
+                ColumnName = dbColumn.ColumnName;
+                ColumnOrdinal = dbColumn.ColumnOrdinal;
+                ColumnSize = dbColumn.ColumnSize;
+                IsAliased = dbColumn.IsAliased;
+                IsAutoIncrement = dbColumn.IsAutoIncrement;
+                IsExpression = dbColumn.IsExpression;
+                IsHidden = dbColumn.IsHidden;
+                IsIdentity = dbColumn.IsIdentity;
+                IsKey = dbColumn.IsKey;
+                IsLong = dbColumn.IsLong;
+                IsReadOnly = dbColumn.IsReadOnly;
+                IsUnique = dbColumn.IsUnique;
+                NumericPrecision = dbColumn.NumericPrecision;
+                NumericScale = dbColumn.NumericScale;
+                UdtAssemblyQualifiedName = dbColumn.UdtAssemblyQualifiedName;
+                DataType = dbColumn.DataType;
+                DataTypeName = dbColumn.DataTypeName;
 
-			foreach (IColumnSchema item in this)
-			{
-				DbColumn dbColumn;
+                // Unfortunately we need this for the property indexer.
+                //  Is it even used?
+                _dbColumn = dbColumn;
+            }
 
-				if (_columnSchemas.TryGetValue(item.ColumnName, out dbColumn))
-					list.Add(dbColumn);
-			}
+            bool IColumnSchema.IsReadOnly
+            {
+                get { return IsReadOnly.GetValueOrDefault(false); }
+            }
 
-			return new ReadOnlyCollection<DbColumn>(list);
-		}
+            bool IColumnSchema.IsIdentity
+            {
+                get { return IsIdentity.GetValueOrDefault(false); }
+            }
 
-		private class ColumnSchema : IColumnSchema
-		{
-			internal ColumnSchema(DbColumn dbColumn)
-			{
-				ColumnName = dbColumn.ColumnName;
-				DataType = dbColumn.DataType;
-				DataTypeName = dbColumn.DataTypeName;
-				IsIdentity = dbColumn.IsIdentity.GetValueOrDefault(false);
-				IsReadOnly = dbColumn.IsReadOnly.GetValueOrDefault(false);
-				IsNullable = dbColumn.AllowDBNull.GetValueOrDefault(true);
-				NumericScale = dbColumn.NumericScale;
-			}
+            bool IColumnSchema.IsNullable
+            {
+                get { return AllowDBNull.GetValueOrDefault(true); }
+            }
 
-			public Type DataType { get; set; }
-			public string DataTypeName { get; set; }
-			public string ColumnName { get; set; }
-			public bool IsReadOnly { get; set; }
-			public bool IsIdentity { get; set; }
-			public bool IsNullable { get; set; }
-			public int? NumericScale { get; set; }
-		}
-	}
+            int? IColumnSchema.NumericScale
+            {
+                get { return NumericScale; }
+                set { NumericScale = value; }
+            }
+
+            public override object this[string property]
+            {
+                get { return _dbColumn[property]; }
+            }
+        }
+    }
 
 #endif
 }
